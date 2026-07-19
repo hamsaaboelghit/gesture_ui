c5d5ef20282188ec3ac0ed568bb9345576925653
HamsaAboelghit <hamsaaboelghit@gmail.com>
2026-07-19 08:24:19 +0300

    Initial setup: venv + opencv/mediapipe/numpy installed

---
+++ test.py
@@ -0,0 +1,7 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+print("OpenCV version:", cv2.__version__)
+print("MediaPipe version:", mp.__version__)
+print("NumPy version:", np.__version__)
